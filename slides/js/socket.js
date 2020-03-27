93d739fcfe897432e9716a5902471bd421e03b28
Chris Durden <cdurden@gmail.com>
2020-03-27 13:26:45 -0500

    adding-getFormData-to-util.js

--- slides/js/socket.js
+++ slides/js/socket.js
@@ -1,13 +1,3 @@
-function getFormData($form){
-    var unindexed_array = $form.serializeArray();
-    var indexed_array = {};
-
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
-    });
-
-    return indexed_array;
-}
 $(document).ready(function() {
     $('form').submit(function (e) {
         socket.emit('form_submit', data=getFormData( $(this) ));
