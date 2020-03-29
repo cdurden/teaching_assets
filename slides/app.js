10c52b4b642faa54a60ebb0242ae639ee07d6130
Chris Durden <cdurden@gmail.com>
2020-03-28 20:18:10 -0500

    edit

--- slides/app.js
+++ slides/app.js
@@ -62,7 +62,7 @@ function init_reveal() {
                 { src: './reveal.js-plugins/anything/anything.js' },
                 { src: './reveal.js/plugin/markdown/marked.js' },
                 { src: './reveal.js/plugin/markdown/markdown.js' },
-                { src: '/static/js/reveal_countdown/countdown.js' },
+                { src: './reveal_countdown/countdown.js' },
                   /*
                 { src: './reveal.js-plugins/broadcast/RTCMultiConnection.min.js'},
                 { src: './reveal.js-plugins/broadcast/socket.io.js'},
