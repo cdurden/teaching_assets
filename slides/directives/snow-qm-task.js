8b934142e6f0a09f86d4e330d5c8e9526f684ba9
Chris Durden <cdurden@gmail.com>
2020-03-29 09:58:33 -0500

    edit

--- slides/directives/snow-qm-task.js
+++ slides/directives/snow-qm-task.js
@@ -17,14 +17,26 @@ function mark(data) {
     //container = $('#snow_qm_'+data.collection+"_"+data.task);
     container = $('#question_'+data.question.id);
     for (let field of data.question.marked_correct) {
-      $(container).find(":input[name='"+field+"'][value='"+data.input_data[field]+"']").mark('<i class="fas fa-check" aria-label="Correct!" title="Correct!"></i>');
+      elmt = $(container).find(":input[name='"+field+"']");
+      if (elmt.length > 1) {
+          elmt = elmt.find(":input[value='"+data.input_data[field]+"']");
+      }
+      elmt.mark('<i class="fas fa-check" aria-label="Correct!" title="Correct!"></i>');
     }
     for (let field of data.question.submitted) {
-      $(container).find(":input[name='"+field+"'][value='"+data.input_data[field]+"']").mark('<i class="fas fa-paper-plane" aria-label="Correct!" title="This answer has been submitted."></i>');
+      elmt = $(container).find(":input[name='"+field+"']");
+      if (elmt.length > 1) {
+          elmt = elmt.find(":input[value='"+data.input_data[field]+"']");
+      }
+      elmt.mark('<i class="fas fa-paper-plane" aria-label="Correct!" title="This answer has been submitted."></i>');
       //$(container).find(":input[name='"+field+"']").mark('<i class="fas fa-paper-plane"></i>');
     }
     for (let field of data.question.marked_incorrect) {
-      $(container).find(":input[name='"+field+"'][value='"+data.input_data[field]+"']").mark('<i class="fas fa-times" aria-label="Correct!" title="Incorrect"></i>');
+      elmt = $(container).find(":input[name='"+field+"']");
+      if (elmt.length > 1) {
+          elmt = elmt.find(":input[value='"+data.input_data[field]+"']");
+      }
+      elmt.mark('<i class="fas fa-times" aria-label="Correct!" title="Incorrect"></i>');
       //$(container).find(":input[name='"+field+"']").mark('<i class="fas fa-times"></i>');
     }
 }
