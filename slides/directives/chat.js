e49432d4896152a9a09ae6034b92dacae66959d4
Chris Durden <cdurden@gmail.com>
2020-03-29 23:34:39 -0500

    chat

--- slides/directives/chat.js
+++ slides/directives/chat.js
@@ -6,18 +6,21 @@ angular.module('slides')
     replace: true,
     templateUrl: './templates/chat.html',
     controller: function () {
-      this.sendMessage = function (ev) {
-        ev.preventDefault(); // prevents page reloading
+      this.sendMessage = function () {
         Sockets.emit('chat-message', ChatData.getInputMessage());
         console.log(ChatData.getInputMessage());
         ChatData.getInput().val('');
         return false;
+      },
+      this.handleSubmit = function(ev) {
+        ev.preventDefault(); // prevents page reloading
+        this.sendMessage();
       }
     },
     link: function (scope, element, attrs, ctrls) {
       var chatCtrl = ctrls[0];
       ChatData.createChat(element);
-      ChatData.getForm().bind("submit",chatCtrl.sendMessage);
+      ChatData.getForm().bind("submit",chatCtrl.handleSubmit);
       ChatData.getInput().on("keypress",function(e) {
           var key = e.keyCode;
           if (key == 13) {
