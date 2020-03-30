3d76537c2ae12219c620ec5491c768a24b10fc84
Chris Durden <cdurden@gmail.com>
2020-03-29 23:01:36 -0500

    chat

--- slides/js/chat.js
+++ slides/js/chat.js
@@ -1,17 +1,17 @@
 function openChatForm() {
-      document.getElementById("chat-popup").style.display = "block";
+    document.getElementById("chat-popup").style.display = "block";
 }
 function closeChatForm() {
-      document.getElementById("chat-popup").style.display = "none";
+    document.getElementById("chat-popup").style.display = "none";
 }
 function toggleChatForm() {
   var x = document.getElementById("chat-popup");
   var btn = document.getElementById("chat-toggle-button");
   if (x.style.display === "none") {
     x.style.display = "block";
-    btn.innerHTML = "Hide chat";
+    //btn.innerHTML = "Hide chat";
   } else {
     x.style.display = "none";
-    btn.innerHTML = "Show chat";
+    //btn.innerHTML = "Show chat";
   }
 }
