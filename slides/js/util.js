d8c49e3f51c099c4f72173ebcbab3367b8955029
Chris Durden <cdurden@gmail.com>
2020-03-30 23:21:56 -0500

    edit

--- slides/js/util.js
+++ slides/js/util.js
@@ -43,3 +43,19 @@ function update_snow_qm_task_data(data) {
     e.preventDefault(); // block the traditional submission of the form.
   });
 }
+function loadScript(url, callback)
+{
+    // adding the script tag to the head as suggested before
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = url;
+
+   // then bind the event to the callback function 
+   // there are several events for cross browser compatibility
+   script.onreadystatechange = callback;
+   script.onload = callback;
+
+   // fire the loading
+   head.appendChild(script);
+}
