db58647d1b8a59a8357d39be0948f9bc31171128
Chris Durden <cdurden@gmail.com>
2020-03-31 09:18:12 -0500

    Revert "save_work"
    This reverts commit c6a7c52faf746f1982b0296b3b1c15e11d461e2c.

--- slides/app.js
+++ slides/app.js
@@ -105,7 +105,6 @@ function init_reveal(deck) {
   });
 }
 var app = angular.module('slides', [
-    'angularLoad',
     'btford.socket-io',
     'services.sockets',
     'slides.services.chatdata',
@@ -325,34 +324,19 @@ app.directive('slideshow', ['$compile', 'Sockets', function($compile, Sockets) {
           } else {
             init_reveal(deck);
           }
-          function save_work () {
-              console.log('saving work');
-              socket.emit('save_work', data=JSON.stringify(RevealChalkboard.storage()));
-          }
-          Reveal.addEventListener( 'ready', function( event ) {
-              RevealChalkboard.loadData('/load_work');
-          });
-          setInterval(save_work,60000);
         }
       });
     }
   };
 }]);
 
-app.controller("myctrl", ["$scope", "$location", "$http", "$routeParams","Sockets","angularLoad", function($scope, $location, $http, $routeParams, Sockets, angularLoad) {
+app.controller("myctrl", ["$scope", "$location", "$http", "$routeParams","Sockets", function($scope, $location, $http, $routeParams, Sockets) {
       Sockets.on('snow_qm_task_data', function (data) {
         console.log(data);
-        Promise.all(data.question.scripts.map(function(script) { 
-            return angularLoad.loadScript(script).then(function(result) { 
-                return result;
-            });
-        })).then(function(results) {
-          // results is an array of names
-          $('#snow_qm_'+data['collection']+'_'+data['task']).html(data.html)
-          $('#snow_qm_'+data['collection']+'_'+data['task']).find('form').submit(function (e) {
-            Sockets.emit('form_submit', data=getFormData( $(this) ));   
-            e.preventDefault(); // block the traditional submission of the form.
-          });
+        $('#snow_qm_'+data['collection']+'_'+data['task']).html(data.html)
+        $('#snow_qm_'+data['collection']+'_'+data['task']).find('form').submit(function (e) {
+          Sockets.emit('form_submit', data=getFormData( $(this) ));   
+          e.preventDefault(); // block the traditional submission of the form.
         });
       });
       Sockets.on('output', function(data) {
