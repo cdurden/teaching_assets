bce0770f48c879cb4da0b3a4493e14cc0688ead1
Chris Durden <cdurden@gmail.com>
2020-04-12 15:37:40 -0500

    edit

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,4 +1,11 @@
 window.MathJax = {
+  config: 'TeX-AMS_HTML-full',
+  tex2jax: {
+    //inlineMath: [ ["\\(","\\)"] ],
+    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+    displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+    processEscapes: true
+  },
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']]
   },
